cc5a863e45686b00e56c3f9a80ca685c89481d85
Dipok Chakraborty <dipokchakraborty@gmail.com>
2021-04-07 20:37:50 +0600

    Initial Commit

---
+++ db.js
@@ -0,0 +1,2 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://jwtrest:N5c02ne1FHYKWI7o@cluster0-shard-00-00.8ww7h.mongodb.net:27017,cluster0-shard-00-01.8ww7h.mongodb.net:27017,cluster0-shard-00-02.8ww7h.mongodb.net:27017/TEST?ssl=true&replicaSet=atlas-yvh7ab-shard-0&authSource=admin&retryWrites=true&w=majority');
